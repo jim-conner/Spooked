d1a63e3c79c7272ad444e11111e5ca5f981bf9b9
jim-conner <jamesdavidconner@gmail.com>
2021-12-07 19:37:17 -0600

    added sql folder and file

---
+++ Spooked/SQL/01_createTables.sql
@@ -0,0 +1,12 @@
+﻿-- create movie table
+Create Table dbo.Movie (
+	Id uniqueidentifier NOT NULL primary key default(newsequentialid()),
+	Title varchar(200) NOT NULL,
+	[Year] varchar(4) NOT NULL,
+	ImdbId varchar(50) NOT NULL,
+	[Type] varchar(20) NOT NULL,
+	Poster nvarchar(4000) NOT NULL,
+	SubGenreId int NOT NULL,
+	Watched bit NOT NULL default 0
+
+);
