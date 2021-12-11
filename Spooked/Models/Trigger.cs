b1d42fe09e366ede0220634eca90c0cee0a8c153
jim-conner <jamesdavidconner@gmail.com>
2021-12-11 14:20:31 -0600

    added models for SubGenre and Trigger

---
+++ Spooked/Models/Trigger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spooked.Models
+{
+    public class Trigger
+    {
+        public Guid Id { get; set; }
+        public string MovieId { get; set; }
+        public string Name { get; set; }
+    }
+}
