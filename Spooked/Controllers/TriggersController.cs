4a189c322f29a096575d936e4044007fc9479c97
jim-conner <jamesdavidconner@gmail.com>
2021-12-17 14:37:54 -0600

    added repo and controller for Triggers

---
+++ Spooked/Controllers/TriggersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Spooked.DataAccess;
+
+namespace Spooked.Controllers
+{
+    [Route("api/triggers")]
+    [ApiController]
+
+    public class TriggersController : ControllerBase
+    {
+        private TriggerRepository _repo;
+
+        public TriggersController(TriggerRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllTriggers()
+        {
+            return Ok(_repo.GetAll());
+        }
+
+        [HttpGet("trigger/{id}")]
+        public IActionResult GetTriggersById(id)
+        {
+            var singleTrigger = _repo.GetById(id);
+
+            if (singleTrigger == null)
+            {
+                return NotFound($"No movie found with MovieId: {id}.");
+            }
+            return Ok(singleTrigger);
+        }
+
+    }
+}
---
+++ Spooked/DataAccess/TriggerRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Spooked.DataAccess
+{
+    public class TriggerRepository
+    {
+        readonly string _connectionString;
+
+        public TriggerRepository(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("Spooked");
+        }
+
+        internal object GetAll()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        internal object GetById(object id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
