f8bacd0867b51f30e289f5c05118f92212d4d596
jim-conner <jamesdavidconner@gmail.com>
2021-12-21 18:29:48 -0600

    added fav div for movie on watchlist to card

--- Spooked.ui/src/App/components/MovieCard.js
+++ Spooked.ui/src/App/components/MovieCard.js
@@ -8,7 +8,15 @@ import MovieDetailModal from './MovieDetailModal';
 function MovieCard({ movieObj }) {
   return (
     <div>
-  <Card color='dark'>
+  <Card color='dark' className='movieCard'>
+    {
+      movieObj.watched === true
+        ? <div className='favBtn'>
+            <i className='fas fa-heart fa-2x' style={{ color: 'orangered' }}></i>
+          </div>
+        : ''
+    }
+
     <CardImg
       alt="Movie Poster"
       src={movieObj.poster}
