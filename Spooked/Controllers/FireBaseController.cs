d7b0909d66b9401a9bbd2bbb6e9f768fff4ca3b0
jim-conner <jamesdavidconner@gmail.com>
2021-12-22 16:20:02 -0600

    updated backend controller

---
+++ Spooked/Controllers/FireBaseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Spooked.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FireBaseController : ControllerBase
+    {
+        public string GetFirebaseUid()
+        {
+            return User.FindFirst(claim => claim.Type == "user_id").Value;
+        }
+
+        public string FirebaseUid => User.FindFirst(claim => claim.Type == "user_id").Value;
+    }
+}
