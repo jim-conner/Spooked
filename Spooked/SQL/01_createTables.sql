41d5d3f1b8942399af1a97564779af52f6be60f4
jim-conner <jamesdavidconner@gmail.com>
2022-01-05 10:47:16 -0600

    re-created db in ssms, updated createTable sql file

--- Spooked/SQL/01_createTables.sql
+++ Spooked/SQL/01_createTables.sql
@@ -1,12 +1,8 @@
-﻿-- create Movie Table
-Create Table dbo.Movie (
-	Id uniqueidentifier NOT NULL primary key default(newsequentialid()),
-	ImdbId varchar(50) NOT NULL,
-	Title varchar(200) NOT NULL,
-	SubGenreId int NOT NULL,
-	Watched bit NOT NULL default 0,
-	Poster nvarchar(2000) NOT NULL
-);
+﻿-- create User Table
+--Create Table dbo.[User] (
+--	Id uniqueidentifier NOT NULL primary key default(newsequentialid()),
+--	FirebaseId varchar(50) NOT NULL,
+--);
 
 -- create SubGenre Table
 Create Table dbo.SubGenre (
@@ -14,28 +10,15 @@ Create Table dbo.SubGenre (
 	[Name] varchar(50) NOT NULL,
 );
 
--- create MovieTrigger Table
-Create Table dbo.[MovieTrigger] (
+-- create Movie Table
+-- Added non-clustered index on SubGenreId & Id via Object Explorer
+Create Table dbo.Movie (
 	Id uniqueidentifier NOT NULL primary key default(newsequentialid()),
-	[Name] varchar(50) NOT NULL,
-	[Value] int NOT NULL,
-	[imdbMovieId] uniqueidentifier NOT NULL
-	Constraint FK_MovieTrigger_Movie Foreign Key ([imdbMovieId])
-		References dbo.[Movie] (Id)
-);
-
--- create Trigger Table
-Create Table dbo.[Trigger] (
-	Id int NOT NULL identity primary key,
-	[Name] varchar(50) NOT NULL,
-	Constraint FK_Trigger_MovieTrigger Foreign Key (Id)
-		References dbo.[MovieTrigger] (triggerId)
-);
-
--- create User Table
-Create Table dbo.[User] (
-	Id uniqueidentifier NOT NULL primary key default(newsequentialid()),
-	FirebaseId varchar(50) NOT NULL,
+	ImdbId varchar(50) NOT NULL,
+	Title varchar(200) NOT NULL,
+	SubGenreId int NOT NULL,
+	Watched bit NOT NULL default 0,
+	Poster nvarchar(2000) NOT NULL,
 );
 
 -- create WatchList Table
@@ -48,3 +31,21 @@ Create Table dbo.[WatchList](
 	Constraint FK_WatchList_Movie Foreign Key (MovieId)
 		References dbo.[Movie] (Id)
 );
+
+-- create Trigger Table
+Create Table dbo.[Trigger] (
+	[Id] int NOT NULL identity primary key,
+	[Name] varchar(50) NOT NULL,
+	[Score] int NOT NULL,
+);
+
+-- create MovieTrigger Table
+-- Added non-clustered index on ImdbMovieId & TriggerId via Object Explorer
+Create Table dbo.[MovieTrigger] (
+	Id uniqueidentifier NOT NULL primary key default(newsequentialid()),
+	[ImdbMovieId] varchar(50) NOT NULL,
+	[TriggerId] int NOT NULL,
+	Constraint FK_MovieTrigger_Trigger Foreign Key (TriggerId)
+		References dbo.[Trigger] (Id)
+);
+
